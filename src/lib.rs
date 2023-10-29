bbec4341b6b15a82a48efa351a246651871e6d73
eggcaker <eggcaker@gmail.com>
2023-10-30 06:39:35 +0800

    add path and sheet name

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,47 @@
-mod to; 
-
+mod to;
 
 use nu_plugin::{EvaluatedCall, LabeledError, Plugin};
-use nu_protocol::{Category, PluginExample, PluginSignature, Type, Value};
-
+use nu_protocol::{Category, PluginExample, PluginSignature, SyntaxShape, Type, Value};
 
 pub struct XLSX;
 
 impl XLSX {
-    pub fn to (&self, call:&EvaluatedCall, input: &Value) -> Result<Value, LabeledError> {
-        let path = match call.positional.get(0) {
-            Some(v) => match v.as_string() {
-            Ok(s) => s,
-            _ => return Err(LabeledError{
-               label:"Expected a path".into(),
-               msg: "Expected a path".into(),
-               span: Some(call.head),
-            }),
-        },
-            None => "UnTittled.xlsx".to_string()
+    pub fn to(&self, call: &EvaluatedCall, input: &Value) -> Result<Value, LabeledError> {
+        let path: String = call.req(0)?;
+
+        let sheet_name = call.opt(1)?.unwrap_or("Sheet1".to_string());
+
+        to::write_to_xlsx(input, &path, &sheet_name)?;
+
+        let val = Value::String {
+            val: String::from(""),
+            internal_span: call.head,
         };
-
-        to::write_to_xlsx(input, &path)?;
-
-        let val = Value::String { val: String::from(""), internal_span: call.head };
         return Ok(val);
-
-       }
-} 
+    }
+}
 
 impl Plugin for XLSX {
     fn signature(&self) -> Vec<PluginSignature> {
-        vec![
-            PluginSignature::build("to xlsx")
+        vec![PluginSignature::build("to xlsx")
             .usage("Convert table to excel(.xlsx) file.")
-            .input_output_type(Type::Record(vec![]), Type::Nothing)
+            .required("file_path", SyntaxShape::String, "required xlsx file path")
+            .optional(
+                "sheet_name",
+                SyntaxShape::String,
+                "Sheet name for the table(default Sheet1)",
+            )
+            .input_output_type(Type::Any, Type::Nothing)
             .plugin_examples(vec![PluginExample {
                 example: "echo [[name]; [bob]] | to xlsx".into(),
-                description: "Converts a table to xlsx".into(),
+                description: "Convert a table or record and save to xlsx file".into(),
                 result: None,
             }])
-            .category(Category::Experimental)
-        ]
+            .category(Category::Experimental)]
     }
 
-
-    fn run (
-        &mut self, 
+    fn run(
+        &mut self,
         name: &str,
         call: &EvaluatedCall,
         input: &Value,
