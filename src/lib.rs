1080b8431bdc48a6b3dabf8374eba125e32d7af1
eggcaker <eggcaker@gmail.com>
2023-11-02 10:17:17 +0800

    add list table data process

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,17 @@ use nu_protocol::{Category, PluginExample, PluginSignature, SyntaxShape, Type, V
 pub struct XLSX;
 
 impl XLSX {
-    pub fn to(&self, call: &EvaluatedCall, input: &Value) -> Result<Value, LabeledError> {
+    pub fn to(&self, call: &EvaluatedCall, value: &Value) -> Result<Value, LabeledError> {
         let path: String = call.req(0)?;
-
         let sheet_name = call.opt(1)?.unwrap_or("Sheet1".to_string());
 
-        to::write_to_xlsx(input, &path, &sheet_name)?;
+        to::write_to_xlsx(call, value, &path, &sheet_name)?;
 
         let val = Value::String {
-            val: String::from(""),
+            val: String::from(format!("The file {} generated.", &path)),
             internal_span: call.head,
         };
+
         return Ok(val);
     }
 }
@@ -24,19 +24,36 @@ impl XLSX {
 impl Plugin for XLSX {
     fn signature(&self) -> Vec<PluginSignature> {
         vec![PluginSignature::build("to xlsx")
-            .usage("Convert table to excel(.xlsx) file.")
+            .usage("Convert table, record, list to excel(.xlsx) file.")
             .required("file_path", SyntaxShape::String, "required xlsx file path")
             .optional(
                 "sheet_name",
                 SyntaxShape::String,
-                "Sheet name for the table(default Sheet1)",
+                "Sheet name, default is Sheet1",
             )
             .input_output_type(Type::Any, Type::Nothing)
-            .plugin_examples(vec![PluginExample {
-                example: "echo [[name]; [bob]] | to xlsx".into(),
-                description: "Convert a table or record and save to xlsx file".into(),
-                result: None,
-            }])
+            .plugin_examples(vec![
+                PluginExample {
+                    example: "{user: bob, age: 30} | to xlsx User.xlsx".into(),
+                    description: "Save a record to xlsx file with sheet name".into(),
+                    result: None,
+                },
+                PluginExample {
+                    example: "echo [[name]; [bob]] | to xlsx".into(),
+                    description: "Save a table to excel xlsx file".into(),
+                    result: None,
+                },
+                PluginExample {
+                    example: "ls | to xlsx Files.xlsx".into(),
+                    description: "Save piped data to xlsx file with sheet name".into(),
+                    result: None,
+                },
+                PluginExample {
+                    example: "[{user: bob, age: 30},{user: john, age:40}] | to xlsx Users.xlsx".into(),
+                    description: "Save list of record to to xlsx file with sheet name".into(),
+                    result: None,
+                },
+            ])
             .category(Category::Experimental)]
     }
 
@@ -47,11 +64,11 @@ impl Plugin for XLSX {
         input: &Value,
     ) -> Result<Value, LabeledError> {
         match name {
-            "to xlsx" => Ok( self.to(call, input)?),
-            _ => Err(LabeledError{
-               label: "Plugin call with wrong name signature".into(),
-               msg: "the signature used to call the plugin does not match any name in the plugin signature vector".into(),
-               span:  Some(call.head),
+            "to xlsx" => Ok(self.to(call, input)?),
+            _ => Err(LabeledError {
+                label: "Plugin call with wrong name signature".into(),
+                msg: "the signature used to call the plugin does not match any name in the plugin signature vector".into(),
+                span: Some(call.head),
             }),
         }
     }
--- src/to.rs
+++ src/to.rs
@@ -1,9 +1,11 @@
-use nu_plugin::LabeledError;
-use nu_protocol::{Span, Value};
+use nu_plugin::{EvaluatedCall, LabeledError};
+use nu_protocol::Value;
 use rust_xlsxwriter::{ColNum, Format, RowNum, Workbook, Worksheet};
 
+
 pub(crate) fn write_to_xlsx(
-    _document: &Value,
+    _call: &EvaluatedCall,
+    value: &Value,
     path: &str,
     sheet_name: &str,
 ) -> Result<(), LabeledError> {
@@ -11,27 +13,28 @@ pub(crate) fn write_to_xlsx(
     let worksheet = workbook.add_worksheet();
     let _ = worksheet.set_name(sheet_name);
 
-    let val = Value::String {
-        val: String::from("hello"),
-        internal_span: Span::unknown(),
-    };
-    let world = Value::String {
-        val: String::from("world"),
-        internal_span: Span::unknown(),
-    };
-    write_value(worksheet, 1, 1, &val).unwrap();
-    write_value(worksheet, 2, 2, &world).unwrap();
-    /*
-    let Value::Record { cols, .. } = document else { todo!() };
+    match &value {
+        Value::List { vals, .. } => {
+            for (row, val) in vals.iter().enumerate() {
+                match val {
+                    Value::Record { val, .. } => {
+                        if row == 0 {
+                            for (column, cn) in val.cols.iter().enumerate() {
+                                let _ = worksheet.write_string(row as RowNum, column as ColNum, cn);
+                            }
+                        }
 
-    for (i, col) in cols.iter().enumerate() {
-        write_value(&mut worksheet, 0, i as u16, Value::String { val: col.clone(), span: Span::unknown() }).unwrap();
-        let data = &document.get_data_by_key(col).unwrap();
-        for (j, val) in data.iter().enumerate() {
-            write_value(&mut worksheet, (j+1) as RowNum, i as ColNum, val).unwrap();
+                        for (column, v) in val.vals.iter().enumerate() {
+                            write_value(worksheet, (row + 1) as RowNum, column as ColNum, &v).unwrap();
+                        }
+                    }
+
+                    _ => {}
+                }
+            }
         }
+        _ => ()
     }
-    */
 
     let _ = workbook.save(path);
 
@@ -46,11 +49,16 @@ fn write_value(
 ) -> Result<(), LabeledError> {
     let _ = match value {
         Value::String { val, .. } => worksheet.write_string(row, col, val),
-        Value::Int { val, .. } => worksheet.write_number(row, col, *val as f64),
+        Value::Int { val, .. } => worksheet.write_number_with_format(row, col, *val as f64, &Format::new().set_num_format("###0")),
         Value::Float { val, .. } => worksheet.write_number(row, col, *val),
+        Value::Date { val, .. } =>
+             worksheet.write_string(row, col, &val.format("%s").to_string()),
+
+        Value::Duration { val, .. } => worksheet.write_string(row, col, val.to_string()),
         Value::Bool { val, .. } => {
             worksheet.write_string(row, col, if *val { "true" } else { "false" })
         }
+        Value::Filesize { val,..} => worksheet.write_string(row, col, val.to_string()),
         Value::Nothing { .. } => worksheet.write_blank(row, col, &Format::new()),
         _ => todo!(),
     };
